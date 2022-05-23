b04552c637998285c0147b7cac368b059a5aec7f
Pedro Prodocimo <pedro.prodocimo@hotmail.com>
2022-05-22 22:58:51 -0300

    contador de tarefas

---
+++ src/components/Lista/index.tsx
@@ -0,0 +1,32 @@
+import style from './Lista.module.scss';
+import Item from './item/index';
+import { ITarefa } from '../../types/tarefa';
+
+
+interface Props{
+    tarefas: ITarefa[],
+    selecionaTarefa: (tarefaSelecionada: ITarefa) => void
+}
+
+
+ function Lista({tarefas , selecionaTarefa} : Props){
+    
+     return (
+         <aside className={style.listaTarefa}>
+             <h2>
+                Tarefas do dia 
+             </h2>
+             <ul>
+                 {tarefas.map(item => (
+                    <Item 
+                    selecionaTarefa={selecionaTarefa}
+                    key={item.id}
+                        {...item}
+                    />
+                 ))}
+             </ul>
+         </aside>
+     )
+ }
+
+ export default Lista;
